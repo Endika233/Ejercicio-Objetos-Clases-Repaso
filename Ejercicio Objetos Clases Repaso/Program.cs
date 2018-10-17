5dfdeadee1e9eeb313f5a91e45facf63c75e8de2
Endika233 <43742370+Endika233@users.noreply.github.com>
2018-10-17 09:54:06 +0200

    Terminado

--- Ejercicio Objetos Clases Repaso/Program.cs
+++ Ejercicio Objetos Clases Repaso/Program.cs
@@ -10,6 +10,12 @@ namespace Ejercicio_Objetos_Clases_Repaso
     {
         static void Main(string[] args)
         {
+            Rectangulo rect1 = new Rectangulo(3,5);
+            Console.WriteLine("El largo es "+rect1.GetLargo());
+            Console.WriteLine("El ancho es "+rect1.GetAncho());
+            Console.WriteLine("El área es "+rect1.Area());
+            Console.WriteLine("La diagonal es "+rect1.Diagonal());
+            Console.ReadKey();
         }
     }
 }
---
+++ Ejercicio Objetos Clases Repaso/Rectangulo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Objetos_Clases_Repaso
+{
+    class Rectangulo
+    {
+        private int largo, ancho;
+        public Rectangulo()
+        {
+
+        }
+        public Rectangulo(int largo,int ancho)
+        {
+            this.largo = largo;
+            this.ancho = ancho;
+        }
+        public void SetLargo(int largo)
+        {
+            this.largo = largo;
+        }
+        public int GetLargo()
+        {
+            return largo;
+        }
+        public void SetAncho(int ancho)
+        {
+            this.ancho = ancho;
+        }
+        public int GetAncho()
+        {
+            return ancho;
+        }
+        public int Area()
+        {
+            return largo * ancho;
+        }
+        public double Diagonal()
+        {
+            return (Math.Sqrt(Math.Pow(Convert.ToDouble(largo), 2)+Math.Pow(Convert.ToDouble(ancho), 2)));
+        }
+    }
+}
